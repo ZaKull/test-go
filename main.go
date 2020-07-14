23f8884b479414a80959eb4a44f7f308ecebccc5
zakull <zakull@rateinc.cl>
2020-07-14 10:41:45 -0400

    - change files to manual google

--- main.go
+++ main.go
@@ -2,30 +2,19 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-	"os/exec"
 )
 
-//a
 func handler(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.CommandContext(r.Context(), "/bin/sh", "script.sh")
-	cmd.Stderr = os.Stderr
-	out, err := cmd.Output()
-	if err != nil {
-		w.WriteHeader(500)
-	}
-	w.Write(out)
+	fmt.Fprintf(w, "<html><head><title>Cloud Run Demo</title></head><body><h1>Greetings from Cloud Run !!! YAY !!!</h1></body></html>\n")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+port, nil)
 }
